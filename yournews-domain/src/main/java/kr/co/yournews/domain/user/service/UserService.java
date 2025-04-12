4a2c72be51215e6042622c49c6bac728b11c5c3e
DaeYoung0726 <pdy000726@naver.com>
2025-04-12 15:54:00 +0900

    feat: User 읽기 로직 구현

---
+++ yournews-domain/src/main/java/kr/co/yournews/domain/user/service/UserService.java
@@ -0,0 +1,18 @@
+package kr.co.yournews.domain.user.service;
+
+import kr.co.yournews.domain.user.entity.User;
+import kr.co.yournews.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public Optional<User> readById(Long id) {
+        return userRepository.findById(id);
+    }
+}
