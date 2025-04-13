97e7a28e759c8907f0fb384e888fcb22969d6200
DaeYoung0726 <pdy000726@naver.com>
2025-04-14 02:45:46 +0900

    feat: JPA Auditing 사용 설정

---
+++ yournews-domain/src/main/java/kr/co/yournews/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package kr.co.yournews.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
