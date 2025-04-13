27c05f359a0c61e4799cf7e93b1b3ec3d738398c
DaeYoung0726 <pdy000726@naver.com>
2025-04-14 02:47:16 +0900

    feat: 회원가입 dto 생성

---
+++ yournews-auth/src/main/java/kr/co/yournews/auth/dto/SignUpDto.java
@@ -0,0 +1,41 @@
+package kr.co.yournews.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import kr.co.yournews.domain.user.entity.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SignUpDto {
+
+    public record Auth(
+            @NotBlank(message = "아이디는 필수 입력입니다.")
+            @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{4,20}$", message = "아이디는 특수문자를 제외한 4~20자리여야 합니다.")
+            String username,
+
+            @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+            @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W).{8,16}", message = "비밀번호는 8~16자 영문, 숫자, 특수문자를 사용하세요.")
+            String password,
+
+            @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+            @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
+            String nickname,
+
+            @NotBlank(message = "이메일은 필수 입력 값입니다.")
+            @Email(message = "이메일 형식이 아닙니다.")
+            String email
+    ) {
+        public String password(PasswordEncoder passwordEncoder) {
+            return passwordEncoder.encode(password);
+        }
+
+        public User toEntity(String encodePassword) {
+            return User.builder()
+                    .username(username)
+                    .password(encodePassword)
+                    .nickname(nickname)
+                    .email(email)
+                    .build();
+        }
+    }
+}
