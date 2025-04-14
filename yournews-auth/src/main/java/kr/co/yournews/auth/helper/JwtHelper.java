e082ef0ffe0a313f25e8790a50b5ed8dc29efd45
DaeYoung0726 <pdy000726@naver.com>
2025-04-14 20:08:53 +0900

    feat: 토큰 재발급 및 삭제 로직 구현

--- yournews-auth/src/main/java/kr/co/yournews/auth/helper/JwtHelper.java
+++ yournews-auth/src/main/java/kr/co/yournews/auth/helper/JwtHelper.java
@@ -1,8 +1,13 @@
 package kr.co.yournews.auth.helper;
 
+import jakarta.servlet.http.HttpServletResponse;
 import kr.co.yournews.auth.dto.TokenDto;
 import kr.co.yournews.auth.jwt.provider.JwtProvider;
 import kr.co.yournews.auth.service.RefreshTokenService;
+import kr.co.yournews.common.exception.AuthErrorType;
+import kr.co.yournews.common.response.exception.CustomException;
+import kr.co.yournews.common.util.AuthConstants;
+import kr.co.yournews.common.util.CookieUtil;
 import kr.co.yournews.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -30,5 +35,51 @@ public class JwtHelper {
         refreshTokenService.saveRefreshToken(username, refreshToken);
 
         return TokenDto.of(accessToken, refreshToken);
+    }
+
+    /**
+     * refreshToken 기반으로 accessToken, refreshToken 재발급
+     *
+     * @param refreshToken : 클라이언트에서 받은 refreshToken (쿠키 등)
+     * @return : 재발급된 토큰을 담은 TokenDto
+     */
+    public TokenDto reissueToken(String refreshToken) {
+        String username = jwtProvider.getUsername(refreshToken);
+
+        if (!refreshTokenService.existedRefreshToken(username))
+            throw new CustomException(AuthErrorType.REFRESH_TOKEN_NOT_FOUND);
+
+        String nickname = jwtProvider.getNickname(refreshToken);
+        Long userId = jwtProvider.getUserId(refreshToken);
+
+        String newAccessToken = jwtProvider.generateAccessToken(username, nickname, userId);
+        String newRefreshToken = jwtProvider.generateRefreshToken(username, nickname, userId);
+
+        refreshTokenService.saveRefreshToken(username, newRefreshToken);
+
+        return TokenDto.of(newAccessToken, newRefreshToken);
+    }
+
+    /**
+     * 로그아웃 시 refreshToken 제거 메서드 (외부에서 호출)
+     *
+     * @param refreshToken : 쿠키에 저장된 refreshToken
+     * @param response     : 쿠키 제거용 HttpServletResponse
+     */
+    public void removeToken(String refreshToken, HttpServletResponse response) {
+        deleteRefreshToken(refreshToken, response);
+    }
+
+    /**
+     * refreshToken 제거 내부 메서드
+     * - 쿠키 삭제 + 저장소(예: Redis)에서 refreshToken 제거
+     *
+     * @param refreshToken : 클라이언트에서 전달받은 refreshToken
+     * @param response     : 쿠키 삭제를 위한 응답 객체
+     */
+    private void deleteRefreshToken(String refreshToken, HttpServletResponse response) {
+        String username = jwtProvider.getUsername(refreshToken);
+        CookieUtil.deleteCookie(AuthConstants.REFRESH_TOKEN_KEY.getValue(), response);
+        refreshTokenService.deleteRefreshToken(username);
     }
 }
