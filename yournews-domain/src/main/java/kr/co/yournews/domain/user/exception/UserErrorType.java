1a27fbec1cf9b99c993c50c0a11c72500d6bc1b4
DaeYoung0726 <pdy000726@naver.com>
2025-04-14 13:47:35 +0900

    feat: 사용자 ErrorType 생성

---
+++ yournews-domain/src/main/java/kr/co/yournews/domain/user/exception/UserErrorType.java
@@ -0,0 +1,35 @@
+package kr.co.yournews.domain.user.exception;
+
+import kr.co.yournews.common.response.error.type.BaseErrorType;
+import kr.co.yournews.common.response.exception.StatusCode;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * User ErrorCode: Uxxx
+ */
+@RequiredArgsConstructor
+public enum UserErrorType implements BaseErrorType {
+
+    NOT_FOUND(StatusCode.NOT_FOUND, "U001", "존재하지 않는 사용자입니다."),
+    NOT_MATCHED_PASSWORD(StatusCode.BAD_REQUEST, "U002", "비밀번호가 일치하지 않습니다."),
+    ;
+
+    private final StatusCode status;
+    private final String code;
+    private final String message;
+
+    @Override
+    public StatusCode getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
