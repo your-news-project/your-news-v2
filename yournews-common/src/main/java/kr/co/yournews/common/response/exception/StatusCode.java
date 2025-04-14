ab04bf1822c3bf86468efe05700b15471bf53bfd
DaeYoung0726 <pdy000726@naver.com>
2025-04-14 13:46:14 +0900

    feat: HTTP 상태 코드 enum 추가

---
+++ yournews-common/src/main/java/kr/co/yournews/common/response/exception/StatusCode.java
@@ -0,0 +1,22 @@
+package kr.co.yournews.common.response.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * HTTP 상태 코드
+ */
+@Getter
+@RequiredArgsConstructor
+public enum StatusCode {
+    OK(200),
+
+    BAD_REQUEST(400),
+    UNAUTHORIZED(401),
+    FORBIDDEN(403),
+    NOT_FOUND(404),
+
+    INTERNAL_SERVER_ERROR(500);
+
+    private final int code;
+}
