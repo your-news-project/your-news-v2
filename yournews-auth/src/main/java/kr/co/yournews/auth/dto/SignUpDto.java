df4cac2e855bed8ca82e3b860ce6aa0238d56bdd
DaeYoung0726 <pdy000726@naver.com>
2025-04-15 20:38:20 +0900

    feat: OAuth 회원가입 dto 추가

--- yournews-auth/src/main/java/kr/co/yournews/auth/dto/SignUpDto.java
+++ yournews-auth/src/main/java/kr/co/yournews/auth/dto/SignUpDto.java
@@ -34,4 +34,10 @@ public class SignUpDto {
                     .build();
         }
     }
+
+    public record OAuth(
+            @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+            @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
+            String nickname
+    ) { }
 }
