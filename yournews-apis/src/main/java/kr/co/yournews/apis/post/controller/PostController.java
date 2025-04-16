11841751876851444c701805a63d9736eb3c0304
DaeYoung0726 <pdy000726@naver.com>
2025-04-16 22:41:39 +0900

    feat: Post 읽기 로직 rest api 연결

--- yournews-apis/src/main/java/kr/co/yournews/apis/post/controller/PostController.java
+++ yournews-apis/src/main/java/kr/co/yournews/apis/post/controller/PostController.java
@@ -3,17 +3,24 @@ package kr.co.yournews.apis.post.controller;
 import jakarta.validation.Valid;
 import kr.co.yournews.apis.post.dto.PostDto;
 import kr.co.yournews.apis.post.service.PostCommandService;
+import kr.co.yournews.apis.post.service.PostQueryService;
 import kr.co.yournews.auth.authentication.CustomUserDetails;
 import kr.co.yournews.common.response.success.SuccessResponse;
+import kr.co.yournews.domain.post.type.Category;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PostController {
     private final PostCommandService postCommandService;
+    private final PostQueryService postQueryService;
 
     @PostMapping
     public ResponseEntity<?> createPost(@AuthenticationPrincipal CustomUserDetails userDetails,
@@ -28,6 +36,25 @@ public class PostController {
         return ResponseEntity.ok(
                 SuccessResponse.from(
                         postCommandService.createPost(userDetails.getUserId(), postDto)
+                )
+        );
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<?> getPostById(@PathVariable Long postId) {
+        return ResponseEntity.ok(
+                SuccessResponse.from(
+                        postQueryService.getPostById(postId)
+                )
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getPostsByCategory(@RequestParam Category category,
+                                                @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return ResponseEntity.ok(
+                SuccessResponse.from(
+                        postQueryService.getPostsByCategory(category, pageable)
                 )
         );
     }
