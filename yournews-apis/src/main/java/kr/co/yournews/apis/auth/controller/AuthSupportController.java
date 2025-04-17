3fbd170e1c7acae45b08d67ac822522145c6188b
DaeYoung0726 <pdy000726@naver.com>
2025-04-18 03:22:10 +0900

    feat: 비밀번호 재설정 로직 rest api 연결

--- yournews-apis/src/main/java/kr/co/yournews/apis/auth/controller/AuthSupportController.java
+++ yournews-apis/src/main/java/kr/co/yournews/apis/auth/controller/AuthSupportController.java
@@ -1,7 +1,10 @@
 package kr.co.yournews.apis.auth.controller;
 
+import jakarta.validation.Valid;
 import kr.co.yournews.apis.auth.dto.AuthCodeDto;
+import kr.co.yournews.apis.auth.dto.PassResetDto;
 import kr.co.yournews.apis.auth.service.mail.AuthCodeManager;
+import kr.co.yournews.apis.auth.service.mail.PassCodeManager;
 import kr.co.yournews.common.response.success.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthSupportController {
     private final AuthCodeManager authCodeManager;
+    private final PassCodeManager passCodeManager;
 
     @PostMapping("/code/request")
     public ResponseEntity<?> requestCode(@RequestBody AuthCodeDto.Request dto) {
@@ -25,6 +29,18 @@ public class AuthSupportController {
     @PostMapping("/code/verify")
     public ResponseEntity<?> verifyCode(@RequestBody AuthCodeDto.Verify dto) {
         authCodeManager.verifyAuthCode(dto);
+        return ResponseEntity.ok(SuccessResponse.ok());
+    }
+
+    @PostMapping("/password/request")
+    public ResponseEntity<?> requestResetLink(@RequestBody PassResetDto.VerifyUser verifyUserDto) {
+        passCodeManager.initiatePasswordReset(verifyUserDto);
+        return ResponseEntity.ok(SuccessResponse.ok());
+    }
+
+    @PostMapping("/password/reset")
+    public ResponseEntity<?> applyNewPassword(@RequestBody @Valid PassResetDto.ResetPassword resetPasswordDto) {
+        passCodeManager.applyNewPassword(resetPasswordDto);
         return ResponseEntity.ok(SuccessResponse.ok());
     }
 }
