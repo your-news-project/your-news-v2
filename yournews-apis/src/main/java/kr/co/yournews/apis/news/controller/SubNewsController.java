0a5706945ed114fc6010659682d614a8742b94e2
DaeYoung0726 <pdy000726@naver.com>
2025-04-19 01:40:37 +0900

    feat: 구독한 소식 조회 rest api 연결

--- yournews-apis/src/main/java/kr/co/yournews/apis/news/controller/SubNewsController.java
+++ yournews-apis/src/main/java/kr/co/yournews/apis/news/controller/SubNewsController.java
@@ -2,11 +2,13 @@ package kr.co.yournews.apis.news.controller;
 
 import kr.co.yournews.apis.news.dto.SubNewsDto;
 import kr.co.yournews.apis.news.service.SubNewsCommandService;
+import kr.co.yournews.apis.news.service.SubNewsQueryService;
 import kr.co.yournews.auth.authentication.CustomUserDetails;
 import kr.co.yournews.common.response.success.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SubNewsController {
     private final SubNewsCommandService subNewsCommandService;
+    private final SubNewsQueryService subNewsQueryService;
+
+    @GetMapping("/subscription")
+    public ResponseEntity<?> getAllSubNews(@AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        return ResponseEntity.ok(
+                SuccessResponse.from(
+                        subNewsQueryService.getAllSubNews(userDetails.getUserId())
+                )
+        );
+    }
 
     @PutMapping("/subscription")
     public ResponseEntity<?> updateSubscriptions(@AuthenticationPrincipal CustomUserDetails userDetails,
