6082438250eaffff248b5041daa495cf02a3b71a
DaeYoung0726 <pdy000726@naver.com>
2025-04-19 00:59:35 +0900

    docs: SubNews Entity 생성

---
+++ yournews-domain/src/main/java/kr/co/yournews/domain/news/entity/SubNews.java
@@ -0,0 +1,37 @@
+package kr.co.yournews.domain.news.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import kr.co.yournews.domain.user.entity.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "sub_news")
+public class SubNews {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "news_id")
+    private News news;
+
+    @Builder
+    public SubNews(User user, News news) {
+        this.user = user;
+        this.news = news;
+    }
+}
