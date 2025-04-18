591f86c24ab43745b4299e3dac926ced13aafc64
DaeYoung0726 <pdy000726@naver.com>
2025-04-19 00:58:11 +0900

    refact: JoinColumn 명시

--- yournews-domain/src/main/java/kr/co/yournews/domain/post/entity/PostLike.java
+++ yournews-domain/src/main/java/kr/co/yournews/domain/post/entity/PostLike.java
@@ -5,6 +5,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import kr.co.yournews.domain.user.entity.User;
 import lombok.AccessLevel;
@@ -21,9 +22,11 @@ public class PostLike {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
     private Post post;
 
     @Builder
