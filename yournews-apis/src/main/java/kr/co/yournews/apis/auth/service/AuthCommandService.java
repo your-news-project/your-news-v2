43eef89d12608fb377d20e20f1dd9ebc150f25b4
DaeYoung0726 <pdy000726@naver.com>
2025-04-18 12:36:57 +0900

    feat: 회원가입 중 인증된 이메일 확인 로직 추가

--- yournews-apis/src/main/java/kr/co/yournews/apis/auth/service/AuthCommandService.java
+++ yournews-apis/src/main/java/kr/co/yournews/apis/auth/service/AuthCommandService.java
@@ -1,6 +1,7 @@
 package kr.co.yournews.apis.auth.service;
 
 import jakarta.servlet.http.HttpServletResponse;
+import kr.co.yournews.apis.auth.service.mail.AuthCodeService;
 import kr.co.yournews.auth.dto.SignInDto;
 import kr.co.yournews.auth.dto.SignUpDto;
 import kr.co.yournews.auth.dto.TokenDto;
@@ -10,11 +11,13 @@ import kr.co.yournews.common.response.exception.CustomException;
 import kr.co.yournews.domain.user.entity.User;
 import kr.co.yournews.domain.user.exception.UserErrorType;
 import kr.co.yournews.domain.user.service.UserService;
+import kr.co.yournews.infra.redis.RedisRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static kr.co.yournews.common.util.AuthConstants.TOKEN_TYPE;
+import static kr.co.yournews.infra.redis.util.RedisConstants.CODE_PREFIX;
 
 @Service
 @RequiredArgsConstructor
@@ -22,15 +25,19 @@ public class AuthCommandService {
     private final UserService userService;
     private final PasswordEncodeService passwordEncodeService;
     private final JwtHelper jwtHelper;
+    private final AuthCodeService authCodeService;
 
     /**
      * dto를 통해 비밀번호 인코딩 후, 회원가입 진행 메서드.
+     * - 인증된 이메일 확인 검증 로직 (외부 api 플랫폼을 통한 회원가입을 막기 위해)
      *
      * @param signUpDto : 사용자 회원가입 요청 dto
      * @return : jwt token
      */
     @Transactional
     public TokenDto signUp(SignUpDto.Auth signUpDto) {
+        authCodeService.ensureVerifiedAndConsume(signUpDto.email());
+
         String encodedPassword = passwordEncodeService.encode(signUpDto.password());
         User user = signUpDto.toEntity(encodedPassword);
         userService.save(user);
