aefa84f44dc4fba363569b55a28520be6566c957
DaeYoung0726 <pdy000726@naver.com>
2025-04-18 22:41:12 +0900

    feat: News CRUD 로직 구현

---
+++ yournews-domain/src/main/java/kr/co/yournews/domain/news/service/NewsService.java
@@ -0,0 +1,31 @@
+package kr.co.yournews.domain.news.service;
+
+import kr.co.yournews.domain.news.entity.News;
+import kr.co.yournews.domain.news.repository.NewsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class NewsService {
+    private final NewsRepository newsRepository;
+
+    public void save(News news) {
+        newsRepository.save(news);
+    }
+
+    public Optional<News> readById(Long id) {
+        return newsRepository.findById(id);
+    }
+
+    public List<News> readAll() {
+        return newsRepository.findAll();
+    }
+
+    public void deleteById(Long id) {
+        newsRepository.deleteById(id);
+    }
+}
