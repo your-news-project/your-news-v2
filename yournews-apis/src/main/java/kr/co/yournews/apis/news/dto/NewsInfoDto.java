ac711b236ff322baab414338156d0570043aea4d
DaeYoung0726 <pdy000726@naver.com>
2025-04-18 22:41:44 +0900

    feat: News 응답 dto 생성

---
+++ yournews-apis/src/main/java/kr/co/yournews/apis/news/dto/NewsInfoDto.java
@@ -0,0 +1,27 @@
+package kr.co.yournews.apis.news.dto;
+
+import kr.co.yournews.domain.news.entity.News;
+
+public class NewsInfoDto {
+
+    public record Summary(
+            Long id,
+            String name,
+            String url
+    ) {
+        public static Summary from(News news) {
+            return new Summary(news.getId(), news.getName(), news.getUrl());
+        }
+    }
+
+    public record Details(
+            Long id,
+            String name,
+            String url
+            // TODO : 구독자 수 추가
+    ) {
+        public static Details from(News news) {
+            return new Details(news.getId(), news.getName(), news.getUrl());
+        }
+    }
+}
