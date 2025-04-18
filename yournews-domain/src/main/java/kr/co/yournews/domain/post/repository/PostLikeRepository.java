70a5881dee4393c1b25425585ad822c18a116947
DaeYoung0726 <pdy000726@naver.com>
2025-04-19 00:58:32 +0900

    refact: 외래키 호출 명시

--- yournews-domain/src/main/java/kr/co/yournews/domain/post/repository/PostLikeRepository.java
+++ yournews-domain/src/main/java/kr/co/yournews/domain/post/repository/PostLikeRepository.java
@@ -4,6 +4,6 @@ import kr.co.yournews.domain.post.entity.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
-    boolean existsByUserIdAndPostId(Long userId, Long postId);
-    void deleteByUserIdAndPostId(Long userId, Long postId);
+    boolean existsByUser_IdAndPost_Id(Long userId, Long postId);
+    void deleteByUser_IdAndPost_Id(Long userId, Long postId);
 }
