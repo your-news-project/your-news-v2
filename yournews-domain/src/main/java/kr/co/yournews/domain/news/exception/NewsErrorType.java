409408f6da0f52f7b56907e552b3a68701d948a9
DaeYoung0726 <pdy000726@naver.com>
2025-04-18 22:41:33 +0900

    feat: News 예외 내용 생성

---
+++ yournews-domain/src/main/java/kr/co/yournews/domain/news/exception/NewsErrorType.java
@@ -0,0 +1,34 @@
+package kr.co.yournews.domain.news.exception;
+
+import kr.co.yournews.common.response.StatusCode;
+import kr.co.yournews.common.response.error.type.BaseErrorType;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * News ErrorCode: Nxxx
+ */
+@RequiredArgsConstructor
+public enum NewsErrorType implements BaseErrorType {
+
+    NOT_FOUND(StatusCode.NOT_FOUND, "N001", "존재하지 않는 소식입니다."),
+    ;
+
+    private final StatusCode status;
+    private final String code;
+    private final String message;
+
+    @Override
+    public StatusCode getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
