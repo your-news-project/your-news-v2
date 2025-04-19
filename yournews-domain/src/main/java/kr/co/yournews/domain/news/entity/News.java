e90fc1a6b3b0d3f3c4f7d67bf0ece17e17c83729
DaeYoung0726 <pdy000726@naver.com>
2025-04-19 18:08:37 +0900

    feat: News Entity에 특정 소식 판단 메서드 구현

--- yournews-domain/src/main/java/kr/co/yournews/domain/news/entity/News.java
+++ yournews-domain/src/main/java/kr/co/yournews/domain/news/entity/News.java
@@ -14,6 +14,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity(name = "news")
 public class News {
+
+    private static final String YU_NEWS_NAME = "영대소식";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,5 +31,14 @@ public class News {
     public News(String name, String url) {
         this.name = name;
         this.url = url;
+    }
+
+    /**
+     * YU 뉴스인지 판단하는 도메인 로직
+     *
+     * @return : name이 영대소식이면 true
+     */
+    public boolean isYuNews() {
+        return YU_NEWS_NAME.equals(this.name);
     }
 }
