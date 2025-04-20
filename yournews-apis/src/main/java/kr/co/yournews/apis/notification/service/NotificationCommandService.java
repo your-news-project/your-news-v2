e8f60110cfa957b054d44e0e757fa6a3c0a10855
DaeYoung0726 <pdy000726@naver.com>
2025-04-21 02:58:21 +0900

    feat: Notification 쓰기 연산 로직 구현

---
+++ yournews-apis/src/main/java/kr/co/yournews/apis/notification/service/NotificationCommandService.java
@@ -0,0 +1,46 @@
+package kr.co.yournews.apis.notification.service;
+
+import kr.co.yournews.common.response.exception.CustomException;
+import kr.co.yournews.domain.notification.entity.Notification;
+import kr.co.yournews.domain.notification.exception.NotificationErrorType;
+import kr.co.yournews.domain.notification.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationCommandService {
+    private final NotificationService notificationService;
+
+    /**
+     * 알림 리스트를 모두 저장하는 메서드
+     * - 알림 발송 후 DB에 저장할 때 사용
+     *
+     * @param notifications : 저장할 알림 목록
+     */
+    @Transactional
+    public void createNotifications(List<Notification> notifications) {
+        notificationService.saveAll(notifications);
+    }
+
+    /**
+     * 특정 알림 id를 기반으로 알림을 삭제
+     *
+     * @param userId         : 사용자 pk
+     * @param notificationId : 삭제할 알림의 pk
+     */
+    @Transactional
+    public void deleteNotification(Long userId, Long notificationId) {
+        Notification notification = notificationService.readById(notificationId)
+                .orElseThrow(() -> new CustomException(NotificationErrorType.NOT_FOUND));
+
+        if (!notification.isReceiver(userId)) {
+            throw new CustomException(NotificationErrorType.FORBIDDEN);
+        }
+
+        notificationService.deleteById(notificationId);
+    }
+}
