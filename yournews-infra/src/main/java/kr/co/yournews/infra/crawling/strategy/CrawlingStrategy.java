e10d63943bdde28eef87f27c4b418733651bbb48
박대영 <125343432+daeyoung0726@users.noreply.github.com>
2025-04-20 16:52:43 +0900

    Feature: 크롤링 게시글 처리 확인을 위한 로직 구현 (#29)
    * chore: domain -> infra 모듈 의존성 추가
    
    * feat: Crawling 전략 ttl 가져오기 메서드 추가
    
    * feat: Redis 상수 추가 및 값 수정
    
    * feat: 크롤링 후 처리된 게시글 확인을 위한 ProcessedUrl 로직 구현

--- yournews-infra/src/main/java/kr/co/yournews/infra/crawling/strategy/CrawlingStrategy.java
+++ yournews-infra/src/main/java/kr/co/yournews/infra/crawling/strategy/CrawlingStrategy.java
@@ -16,4 +16,5 @@ public interface CrawlingStrategy {
     List<String> getSubscribedMembers(String newsName);
     void saveUrl(String postUrl);
     boolean isExisted(String postUrl);
+    long getTTL();
 }
--- yournews-infra/src/main/java/kr/co/yournews/infra/redis/util/RedisConstants.java
+++ yournews-infra/src/main/java/kr/co/yournews/infra/redis/util/RedisConstants.java
@@ -4,9 +4,10 @@ public final class RedisConstants {
 
     private RedisConstants() { }
 
-    public static final String REFRESH_TOKEN_PREFIX = "RT::";
-    public static final String CODE_PREFIX = "CD::";
-    public static final String VERIFIED_PREFIX = "VERIFY::";
-    public static final String BLACKLIST_KEY_PREFIX = "BL::";
-    public static final String PASS_KEY_PREFIX = "PASS::";
+    public static final String REFRESH_TOKEN_PREFIX = "auth:refresh::";
+    public static final String CODE_PREFIX = "auth:code::";
+    public static final String VERIFIED_PREFIX = "auth:verified::";
+    public static final String BLACKLIST_KEY_PREFIX = "auth:blacklist::";
+    public static final String PASS_KEY_PREFIX = "auth:passcode::";
+    public static final String PROCESSED_URL_PREFIX = "processed-url::";
 }
