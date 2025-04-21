ec835def4b22ee9475842cbaf5a9e77ae8670539
DaeYoung0726 <pdy000726@naver.com>
2025-04-21 16:57:33 +0900

    feat: 아이디 및 닉네임 존재 확인 rest api 연결

--- yournews-apis/src/main/java/kr/co/yournews/apis/auth/controller/AuthSupportController.java
+++ yournews-apis/src/main/java/kr/co/yournews/apis/auth/controller/AuthSupportController.java
@@ -4,15 +4,18 @@ import jakarta.validation.Valid;
 import kr.co.yournews.apis.auth.dto.AuthCodeDto;
 import kr.co.yournews.apis.auth.dto.PassResetDto;
 import kr.co.yournews.apis.auth.dto.UsernameDto;
+import kr.co.yournews.apis.auth.service.UserValidationService;
 import kr.co.yournews.apis.auth.service.UsernameFindService;
 import kr.co.yournews.apis.auth.service.mail.AuthCodeManager;
 import kr.co.yournews.apis.auth.service.mail.PassCodeManager;
 import kr.co.yournews.common.response.success.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +25,7 @@ public class AuthSupportController {
     private final AuthCodeManager authCodeManager;
     private final PassCodeManager passCodeManager;
     private final UsernameFindService usernameFindService;
+    private final UserValidationService userValidationService;
 
     @PostMapping("/code/request")
     public ResponseEntity<?> requestCode(@RequestBody AuthCodeDto.Request dto) {
@@ -56,4 +60,13 @@ public class AuthSupportController {
         );
     }
 
+    @GetMapping("/check-username")
+    public ResponseEntity<?> checkUsername(@RequestParam String value) {
+        return ResponseEntity.ok(SuccessResponse.from(userValidationService.isUsernameExists(value)));
+    }
+
+    @GetMapping("/check-nickname")
+    public ResponseEntity<?> checkNickname(@RequestParam String value) {
+        return ResponseEntity.ok(SuccessResponse.from(userValidationService.isNicknameExists(value)));
+    }
 }
