22b7610c71a9b74fe5f30f094e723165838fd197
DaeYoung0726 <pdy000726@naver.com>
2025-04-23 14:00:49 +0900

    feat: FcmToken CRUD 구현

---
+++ yournews-domain/src/main/java/kr/co/yournews/domain/user/service/FcmTokenService.java
@@ -0,0 +1,39 @@
+package kr.co.yournews.domain.user.service;
+
+import kr.co.yournews.domain.user.entity.FcmToken;
+import kr.co.yournews.domain.user.repository.FcmTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class FcmTokenService {
+    private final FcmTokenRepository fcmTokenRepository;
+
+    public void save(FcmToken fcmToken) {
+        fcmTokenRepository.save(fcmToken);
+    }
+
+    public List<FcmToken> readAllByUserId(Long userId) {
+        return fcmTokenRepository.findAllByUser_Id(userId);
+    }
+
+    public Optional<FcmToken> readAllByUserIdAndDeviceInfo(Long userId, String deviceInfo) {
+        return fcmTokenRepository.findByUser_IdAndDeviceInfo(userId, deviceInfo);
+    }
+
+    public void deleteByToken(String token) {
+        fcmTokenRepository.deleteByToken(token);
+    }
+
+    public void deleteAllByUserId(Long userId) {
+        fcmTokenRepository.deleteAllByUser_Id(userId);
+    }
+
+    public void deleteByUserIdAndDeviceInfo(Long userId, String deviceInfo) {
+        fcmTokenRepository.deleteByUser_IdAndDeviceInfo(userId, deviceInfo);
+    }
+}
