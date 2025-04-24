b17ff47e042f219e977bfc8fdc11cc455c03508d
DaeYoung0726 <pdy000726@naver.com>
2025-04-25 02:59:57 +0900

    feat: RabbitMQ 환경변수 주입

---
+++ yournews-infra/src/main/java/kr/co/yournews/infra/properties/RabbitMqProperties.java
@@ -0,0 +1,15 @@
+package kr.co.yournews.infra.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "rabbitmq")
+@Getter @Setter
+public class RabbitMqProperties {
+    private String queueName;
+    private String exchangeName;
+    private String routingKey;
+}
