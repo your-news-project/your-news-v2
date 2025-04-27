b71c2ebcb4682b85fbf3413683947893d898f084
DaeYoung0726 <pdy000726@naver.com>
2025-04-28 02:17:36 +0900

    feat: 시간 기반 알림 삭제 로직 추가

--- yournews-domain/src/main/java/kr/co/yournews/domain/notification/repository/NotificationRepository.java
+++ yournews-domain/src/main/java/kr/co/yournews/domain/notification/repository/NotificationRepository.java
@@ -4,11 +4,19 @@ import kr.co.yournews.domain.notification.entity.Notification;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>, CustomNotificationRepository {
     Optional<Notification> findByUserIdAndPublicId(Long userId, String publicId);
     Page<Notification> findAllByUserId(Long userId, Pageable pageable);
     Page<Notification> findAllByUserIdAndIsRead(Long userId, boolean isRead, Pageable pageable);
+
+    @Modifying
+    @Query("DELETE FROM notification n WHERE n.createdAt < :dateTime")
+    void deleteByDateTimeBefore(@Param("dateTime") LocalDate dateTime);
 }
--- yournews-domain/src/main/java/kr/co/yournews/domain/notification/service/NotificationService.java
+++ yournews-domain/src/main/java/kr/co/yournews/domain/notification/service/NotificationService.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,5 +38,9 @@ public class NotificationService {
 
     public void deleteById(Long id) {
         notificationRepository.deleteById(id);
+    }
+
+    public void deleteByDateTime(LocalDate dateTime) {
+        notificationRepository.deleteByDateTimeBefore(dateTime);
     }
 }
