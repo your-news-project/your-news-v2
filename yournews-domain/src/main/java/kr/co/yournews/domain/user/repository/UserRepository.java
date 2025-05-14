7eb5c994249f11470309551a4e6d8547fae9b3a3
DaeYoung0726 <pdy000726@naver.com>
2025-05-15 02:56:59 +0900

    refact: jpql 로직 queryDsl로 변환

--- yournews-domain/src/main/java/kr/co/yournews/domain/user/repository/UserRepository.java
+++ yournews-domain/src/main/java/kr/co/yournews/domain/user/repository/UserRepository.java
@@ -10,22 +10,13 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, CustomUserRepository {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
     boolean existsByNickname(String nickname);
     boolean existsByUsernameAndEmail(String username, String email);
-
-    @Query("""
-                SELECT u.id
-                FROM user u
-                JOIN sub_news sn ON u.id = sn.user.id
-                WHERE u.subStatus = true
-                  AND sn.newsName = :newsName
-            """)
-    List<Long> findUserIdsByNewsNameAndSubStatusTrue(@Param("newsName") String newsName);
 
     @Query(value = "SELECT * FROM user WHERE username = :username", nativeQuery = true)
     Optional<User> findByUsernameIncludeDeleted(@Param("username") String username);
