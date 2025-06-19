6f4727385efb050526b0344211d827387bf5b425
DaeYoung0726 <pdy000726@naver.com>
2025-06-20 01:50:40 +0900

    refact: Fcm Dto 필드명 변경

--- yournews-apis/src/main/java/kr/co/yournews/apis/fcm/dto/FcmMessageDto.java
+++ yournews-apis/src/main/java/kr/co/yournews/apis/fcm/dto/FcmMessageDto.java
@@ -3,9 +3,9 @@ package kr.co.yournews.apis.fcm.dto;
 public record FcmMessageDto(
         String token,
         String title,
-        String content
+        String data
 ) {
-    public static FcmMessageDto of(String token, String title, String content) {
-        return new FcmMessageDto(token, title, content);
+    public static FcmMessageDto of(String token, String title, String data) {
+        return new FcmMessageDto(token, title, data);
     }
 }
