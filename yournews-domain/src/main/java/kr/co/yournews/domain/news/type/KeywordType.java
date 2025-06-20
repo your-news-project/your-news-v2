e426f28d10f8628e5ae9cdd92db20f8c39abe248
DaeYoung0726 <pdy000726@naver.com>
2025-06-20 14:03:24 +0900

    fix: 소식 키워드 추출 과정에서 예외처리 제거 (예외 발생 시, 동작 종료 방지)

--- yournews-domain/src/main/java/kr/co/yournews/domain/news/type/KeywordType.java
+++ yournews-domain/src/main/java/kr/co/yournews/domain/news/type/KeywordType.java
@@ -1,13 +1,13 @@
 package kr.co.yournews.domain.news.type;
 
-import kr.co.yournews.common.response.exception.CustomException;
-import kr.co.yournews.domain.news.exception.NewsErrorType;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Getter
 @RequiredArgsConstructor
 public enum KeywordType {
@@ -16,7 +16,8 @@ public enum KeywordType {
     EDUCATION("교육 및 강좌"),
     WELFARE("학생복지"),
     RESEARCH("연구 및 프로젝트 참여"),
-    EVENT("행사 및 설명회");
+    EVENT("행사 및 설명회"),
+    ETC("기타");
 
     private final String label;
 
@@ -30,15 +31,16 @@ public enum KeywordType {
 
     /**
      * 전달된 문자열 라벨에 해당하는 KeywordType enum을 반환
+     * - 유효하지 않은 키워드일 경우 ETC 반환 (해당 게시글을 제외하고 진행하기 위함)
      *
      * @param label : 클라이언트가 전달한 키워드 문자열
      * @return : 해당 키워드에 대응하는 KeywordType enum
-     * @throws CustomException INVALID_KEYWORD : 유효하지 않은 키워드일 경우 예외 발생
      */
     public static KeywordType fromLabel(String label) {
         KeywordType keywordType = LABEL_MAP.get(label);
         if (keywordType == null) {
-            throw new CustomException(NewsErrorType.INVALID_KEYWORD);
+            log.warn("지원하지 않는 키워드 : {}", label);
+            return ETC;
         }
         return keywordType;
     }
