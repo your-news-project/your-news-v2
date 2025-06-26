e8058bdd8d3cdfaf8c48dab92173c7c4ddd3bde9
DaeYoung0726 <pdy000726@naver.com>
2025-06-27 00:58:21 +0900

    refact: API prefix 변경

--- yournews-admin/src/main/java/kr/co/yournews/admin/auth/controller/AuthController.java
+++ yournews-admin/src/main/java/kr/co/yournews/admin/auth/controller/AuthController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/admin/auth")
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/admin")
 public class AuthController {
     private final AuthService authService;
 
