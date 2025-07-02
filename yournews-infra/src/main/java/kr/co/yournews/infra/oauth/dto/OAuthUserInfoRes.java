2813d42f716de27faac9e5f2c22da312903a32b2
DaeYoung0726 <pdy000726@naver.com>
2025-07-02 17:07:50 +0900

    refact: OAuth 사용자 정보 필드 변경

--- yournews-infra/src/main/java/kr/co/yournews/infra/oauth/dto/OAuthUserInfoRes.java
+++ yournews-infra/src/main/java/kr/co/yournews/infra/oauth/dto/OAuthUserInfoRes.java
@@ -2,10 +2,9 @@ package kr.co.yournews.infra.oauth.dto;
 
 public record OAuthUserInfoRes(
         String id,
-        String nickname,
         String email
 ) {
-    public static OAuthUserInfoRes of(String id, String nickname, String email) {
-        return new OAuthUserInfoRes(id, nickname, email);
+    public static OAuthUserInfoRes of(String id, String email) {
+        return new OAuthUserInfoRes(id, email);
     }
 }
