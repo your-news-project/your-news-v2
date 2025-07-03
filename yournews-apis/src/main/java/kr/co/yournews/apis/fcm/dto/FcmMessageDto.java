9452a618da0e1a8e1958801f6bbc76c75a83ae82
DaeYoung0726 <pdy000726@naver.com>
2025-07-04 02:40:44 +0900

    feat: 마지막 메시지임을 확인하기 위한 필드 추가 (메시지 브로커 컨슈머가 마지막을 추정하기 위함)

--- yournews-apis/src/main/java/kr/co/yournews/apis/fcm/dto/FcmMessageDto.java
+++ yournews-apis/src/main/java/kr/co/yournews/apis/fcm/dto/FcmMessageDto.java
@@ -3,9 +3,10 @@ package kr.co.yournews.apis.fcm.dto;
 public record FcmMessageDto(
         String token,
         String title,
-        String data
+        String data,
+        boolean isLast
 ) {
-    public static FcmMessageDto of(String token, String title, String data) {
-        return new FcmMessageDto(token, title, data);
+    public static FcmMessageDto of(String token, String title, String data, boolean isLast) {
+        return new FcmMessageDto(token, title, data, isLast);
     }
 }
