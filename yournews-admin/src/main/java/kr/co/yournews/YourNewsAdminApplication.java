6fe5c753496a3fc5108e7ecfe040873779346c01
DaeYoung0726 <pdy000726@naver.com>
2025-07-06 16:10:30 +0900

    admin cd test

--- yournews-admin/src/main/java/kr/co/yournews/YourNewsAdminApplication.java
+++ yournews-admin/src/main/java/kr/co/yournews/YourNewsAdminApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YourNewsAdminApplication {
 
+    // test
     public static void main(String[] args) {
         SpringApplication.run(YourNewsAdminApplication.class, args);
     }
