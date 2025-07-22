e925306563e92fea142201bda8bfd24e7d5d5ae5
DaeYoung0726 <pdy000726@naver.com>
2025-07-22 18:12:55 +0900

    refact: SringBootTest 애너테이션 제거

--- yournews-apis/src/test/java/kr/co/yournews/apis/YournewsApisApplicationTests.java
+++ yournews-apis/src/test/java/kr/co/yournews/apis/YournewsApisApplicationTests.java
@@ -1,9 +1,7 @@
 package kr.co.yournews.apis;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class YournewsApisApplicationTests {
 
 	@Test
