71f00009265f28f81d7ba70f11a002673e8cdda1
DaeYoung0726 <pdy000726@naver.com>
2025-07-22 18:11:52 +0900

    refact: 불필요 dto 정적 메서드 제거

--- yournews-apis/src/main/java/kr/co/yournews/apis/notification/dto/NotificationRankingDto.java
+++ yournews-apis/src/main/java/kr/co/yournews/apis/notification/dto/NotificationRankingDto.java
@@ -4,7 +4,4 @@ public record NotificationRankingDto(
         String name,
         int score
 ) {
-    public NotificationRankingDto of(String name, int score) {
-        return new NotificationRankingDto(name, score);
-    }
 }
