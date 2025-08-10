86a6665219a87c8f538125ec0fb738feab700dcc
DaeYoung0726 <pdy000726@naver.com>
2025-08-11 01:43:31 +0900

    refact: FCM 처리 코드 삭제

--- yournews-infra/src/main/java/kr/co/yournews/infra/fcm/exception/FcmSendFailureException.java
+++
@@ -1,7 +0,0 @@
-package kr.co.yournews.infra.fcm.exception;
-
-public class FcmSendFailureException extends RuntimeException {
-    public FcmSendFailureException(String message) {
-        super(message);
-    }
-}
