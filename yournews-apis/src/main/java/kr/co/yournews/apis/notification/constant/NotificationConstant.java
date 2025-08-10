a95e574f835181d3ea17aa38a0e379463f89fbcc
DaeYoung0726 <pdy000726@naver.com>
2025-08-11 01:45:24 +0900

    refact: 불필요 로직 삭제 및 클래스명 변경

--- yournews-apis/src/main/java/kr/co/yournews/apis/notification/constant/NotificationConstant.java
+++ yournews-apis/src/main/java/kr/co/yournews/apis/notification/constant/NotificationConstant.java
@@ -1,11 +1,10 @@
-package kr.co.yournews.infra.fcm.constant;
+package kr.co.yournews.apis.notification.constant;
 
-public final class FcmConstant {
-    private FcmConstant() { }
+public final class NotificationConstant {
+    private NotificationConstant() { }
 
     private static final String NEWS_NOTIFICATION_TITLE = "[%s] 새로운 소식이 도착했습니다!";
     private static final String DAILY_NEWS_NOTIFICATION_TITLE = "[%s] 일간 소식이 도착했습니다!";
-    public static final String NEWS_NOTIFICATION_CONTENT = "새로운 소식을 확인해보세요!";
 
     public static String getNewsNotificationTitle(String newsName) {
         return String.format(NEWS_NOTIFICATION_TITLE, newsName);
