0f26436d215930da93d7f1d3e88b1f0b8aa4b322
DaeYoung0726 <pdy000726@naver.com>
2025-12-19 18:16:42 +0900

    feat: 구독 환경변수 주입

---
+++ yournews-infra/src/main/java/kr/co/yournews/infra/iap/config/AppleIapProperties.java
@@ -0,0 +1,18 @@
+package kr.co.yournews.infra.iap.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "apple-iap")
+@Getter @Setter
+public class AppleIapProperties {
+    private String issuerId;
+    private String keyId;
+    private String bundleId;
+    private String privateKey;
+    private Long appleId;
+    private String environment;
+}
