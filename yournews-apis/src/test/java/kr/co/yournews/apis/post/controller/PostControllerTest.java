d19ac3d86a9ed2ea7d70e0d76fc3ff4f9ccf6bdb
DaeYoung0726 <pdy000726@naver.com>
2025-12-20 23:58:47 +0900

    test: 최신 공지사항 조회 rest api test code 작성

--- yournews-apis/src/test/java/kr/co/yournews/apis/post/controller/PostControllerTest.java
+++ yournews-apis/src/test/java/kr/co/yournews/apis/post/controller/PostControllerTest.java
@@ -6,6 +6,7 @@ import kr.co.yournews.apis.post.service.PostQueryService;
 import kr.co.yournews.domain.post.type.Category;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -80,5 +81,54 @@ public class PostControllerTest {
                 .andExpect(jsonPath("$.data.content[0].title").value(postInfoDtos.getContent().get(0).title()))
                 .andExpect(jsonPath("$.data.content[1].title").value(postInfoDtos.getContent().get(1).title()))
                 .andExpect(jsonPath("$.data.content[2].title").value(postInfoDtos.getContent().get(2).title()));
+    }
+
+    @Nested
+    @DisplayName("최신 공지사항 조회 테스트")
+    class GetLatestNotice {
+
+        @Test
+        @DisplayName("공지사항이 존재 - id와 title을 반환")
+        void getLatestNoticeExists() throws Exception {
+            // given
+            PostInfoDto.Preview preview = new PostInfoDto.Preview(1L, "최신 공지");
+
+            given(postQueryService.getLatestNotice()).willReturn(preview);
+
+            // when
+            ResultActions resultActions = mockMvc.perform(
+                    get("/api/v1/posts/notices/latest")
+            );
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.message").value("요청이 성공하였습니다."))
+                    .andExpect(jsonPath("$.data.id").value(1))
+                    .andExpect(jsonPath("$.data.title").value("최신 공지"));
+        }
+
+        @Test
+        @DisplayName("공지사항이 존재 x - null 반환")
+        void getLatestNoticeEmpty() throws Exception {
+            // given
+            PostInfoDto.Preview empty = PostInfoDto.Preview.empty();
+
+            given(postQueryService.getLatestNotice()).willReturn(empty);
+
+            // when
+            ResultActions resultActions = mockMvc.perform(
+                    get("/api/v1/posts/notices/latest")
+            );
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.message").value("요청이 성공하였습니다."))
+                    .andExpect(jsonPath("$.data.id").isEmpty())
+                    .andExpect(jsonPath("$.data.title").isEmpty());
+        }
     }
 }
