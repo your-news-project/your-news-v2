9e707d7eec889a8d73885af78a76e731c6a31a5f
DaeYoung0726 <pdy000726@naver.com>
2026-01-28 13:12:55 +0900

    feat: RabbitMQ ACK/NACK 콜백 등록 * 순환 참조 방지를 위한 지연 조회 처리

--- yournews-infra/src/main/java/kr/co/yournews/infra/config/RabbitMqConfig.java
+++ yournews-infra/src/main/java/kr/co/yournews/infra/config/RabbitMqConfig.java
@@ -1,25 +1,36 @@
 package kr.co.yournews.infra.config;
 
-import lombok.RequiredArgsConstructor;
+import kr.co.yournews.infra.rabbitmq.RabbitCallbacks;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
-@RequiredArgsConstructor
 public class RabbitMqConfig {
 
     /**
      * RabbitTemplate 설정
      */
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
-        return rabbitTemplate;
+    public RabbitTemplate rabbitTemplate(
+            ConnectionFactory connectionFactory,
+            MessageConverter messageConverter,
+            ObjectProvider<RabbitCallbacks> provider
+    ) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+
+        // 메시지 브로커 발행 ACK/NACK 콜백 등록 (순환참조 방지용 지연 조회)
+        template.setConfirmCallback((correlationData, ack, cause) ->
+                provider.getObject().handleConfirm(correlationData, ack, cause)
+        );
+
+        return template;
     }
 
     /**
