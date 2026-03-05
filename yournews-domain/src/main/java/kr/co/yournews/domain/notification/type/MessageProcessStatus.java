c456c893033a9a78267765562b6b93f909450cf3
DaeYoung0726 <pdy000726@naver.com>
2026-03-06 01:44:04 +0900

    docs: 메시지 처리 상태 주석 추가

--- yournews-domain/src/main/java/kr/co/yournews/domain/notification/type/MessageProcessStatus.java
+++ yournews-domain/src/main/java/kr/co/yournews/domain/notification/type/MessageProcessStatus.java
@@ -1,9 +1,9 @@
 package kr.co.yournews.domain.notification.type;
 
 public enum MessageProcessStatus {
-    PROCESSING,
-    RETRY_PENDING,
-    SUCCEEDED,
-    FAILED_RETRY_EXHAUSTED,
-    FAILED_PERMANENT
+    PROCESSING,                 // 처리중
+    RETRY_PENDING,              // 재시도 대기
+    SUCCEEDED,                  // 처리 성공
+    FAILED_RETRY_EXHAUSTED,     // 재시도 횟수 초과
+    FAILED_PERMANENT            // 영구적인 실패 (ex. 토큰 존재 x)
 }
